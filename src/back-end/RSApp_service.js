1789ddd0b88530c50f820128377261726b1dab75
Jon Thomas <jrthomas88@gmail.com>
2018-11-27 18:05:15 -0700

    Created starting files

---
+++ src/back-end/RSApp_service.js
@@ -0,0 +1,17 @@
+/**
+
+ RSApp_service.js
+ Author: Jon Thomas
+ Class: CSC-337 Fall 2018
+
+ Back-end service for RSApp.  Performs the following services for the
+ front-end component:
+
+ 1. Retrieves public keys from the MySQL database
+ 2. Stores new keys in the MySQL database
+ 3. Perform encryption using a public key
+ 4. Perform decryption using a provided private key
+ 5. Generate prime numbers
+ 6. Find a factor of a provided number
+
+ **/
---
+++ src/front-end/RSApp.js
@@ -0,0 +1,21 @@
+/**
+
+ RSApp.js
+ Author: Jon Thomas
+ Class: CSC-337 Fall 2018
+ Instructor: Allison Obourn
+
+ Front-end JavaScript for RSApp.html.  Handles collecting user input,
+ sends data to Node.js back-end code for security, encryption, decryption, etc.
+
+ **/
+
+"use strict";
+
+(function () {
+
+    window.onload = function () {
+
+    };
+
+})();
